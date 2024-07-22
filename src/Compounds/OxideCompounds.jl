96bcffee6a90587c40737dab96d3370fddf5b14c
Sush1090 <sushrut1090@gmail.com>
2024-07-22 13:51:00 +0200

    Sio2 now needs string input

--- src/Compounds/OxideCompounds.jl
+++ src/Compounds/OxideCompounds.jl
@@ -114,47 +114,42 @@ struct SiO₂ <: OxideCompounds
     MolarMass
     T_range::Vector{Tuple}
     PieceWise::Bool
-    function SiO₂(;name = "QUARTZ",A = [8.145E+01 1.828E-03 -1.810E+05 -6.985E+02 5.406E-06;
-                                        5.796E+01 9.330E-03 1.835E+0 0 0],MolarMass = 60.084,T_range = [(298,844),(844,1700)],PieceWise = true)
-        #if name == "QUARTZ"
-            if PieceWise ==false
+    function SiO₂(name = "QUARTZ";A = [8.145E+01 1.828E-03 -1.810E+05 -6.985E+02 5.406E-06;
+                                        5.796E+01 9.330E-03 1.835E+6 0 0],MolarMass = 60.084,T_range = [(298,844),(844,1700)],PieceWise = true)
+        if name == "QUARTZ"
                 @assert size(A,1) == size(T_range,1) 
-                new(name,A,MolarMass,T_range,PieceWise)
-            else 
-                @info "Compound with Piecewise entries" 
-                new(name,A,MolarMass,T_range,PieceWise)
-            end
-        #end
+                return new(name,A,MolarMass,T_range,PieceWise)
+        end
 
-        # if name == "CRISTOBALITE"
-        #     A = [-4.160E+03 2.548E+00 -6.286E+07 7.168E+0 0;7.275E+01 1.300E-03 -4.132E+06  0 0];
-        #     T_range = [(298,523),(523,1800)] 
-        #     PieceWise = true
-        #     @assert size(A,1) == size(T_range,1) 
-        #     new(name,A,MolarMass,T_range,PieceWise)
-        # end
+        if name == "CRISTOBALITE"
+            A = [-4.160E+03 2.548E+00 -6.286E+07 7.168E+0 0;7.275E+01 1.300E-03 -4.132E+06  0 0];
+            T_range = [(298,523),(523,1800)] 
+            PieceWise = true
+            @assert size(A,1) == size(T_range,1) 
+            return new(name,A,MolarMass,T_range,PieceWise)
+        end
 
-        # if name == "COESITE"
-        #     A = [2.331E+02 -7.777E-02 2.604E+06 -3.375E+03 1.924E-05];
-        #     T_range = [(298,1800)] 
-        #     PieceWise = true
-        #     @assert size(A,1) == size(T_range,1) 
-        #     new(name,A,MolarMass,T_range,PieceWise)   
-        # end
+        if name == "COESITE"
+            A = [2.331E+02 -7.777E-02 2.604E+06 -3.375E+03 1.924E-05];
+            T_range = [(298,1800)] 
+            PieceWise = true
+            @assert size(A,1) == size(T_range,1) 
+            return new(name,A,MolarMass,T_range,PieceWise)   
+        end
 
-        # if name == "STISHOVITE"
-        #     A = [1.470E+02 -4.027E-02 -2.834E+05 -1.559E+03 1.203E-05];
-        #     T_range = [(298,1800)] 
-        #     PieceWise = true 
-        #     new(name,A,MolarMass,T_range,PieceWise)       
-        # end
+        if name == "STISHOVITE"
+            A = [1.470E+02 -4.027E-02 -2.834E+05 -1.559E+03 1.203E-05];
+            T_range = [(298,1800)] 
+            PieceWise = true 
+            return new(name,A,MolarMass,T_range,PieceWise)       
+        end
 
-        # if name == "SILICA GLASS"
-        #     A = [1.27200E+02 -1.0777E-02 4.3127E+05 -1.4638E+03 0];
-        #     T_range = [(298,1700)] 
-        #     PieceWise = false 
-        #     new(name,A,MolarMass,T_range,PieceWise)       
-        # end
+       if name == "SILICAGLASS"
+            A = [1.27200E+02 -1.0777E-02 4.3127E+05 -1.4638E+03 0];
+            T_range = [(298,1700)] 
+            PieceWise = false 
+            return new(name,A,MolarMass,T_range,PieceWise)       
+        end
 
     end
 end
@@ -166,8 +161,8 @@ struct Fe₂O₃ <: OxideCompounds
     MolarMass
     T_range::Vector{Tuple}
     PieceWise::Bool
-    function Fe₂O₃(;name = "HEMATITE",A = [8.378E+02 -6.656E-01 4.989E+06 -1.075E+0 3.487E-04;
-                                            -1.096E+03 2.727E-01 -1.024E+08 3.396E+0 0],MolarMass = 159.692,T_range = [(298,950),(950,1800)],PieceWise = true)
+    function Fe₂O₃(;name = "HEMATITE",A = [8.378E+02 -6.656E-01 4.989E+06 -1.075E+04 3.487E-04;
+                                            -1.096E+03 2.727E-01 -1.024E+08 3.396E+04 0],MolarMass = 159.692,T_range = [(298,950),(950,1800)],PieceWise = true)
             @assert size(A,1) == size(T_range,1) 
             new(name,A,MolarMass,T_range,PieceWise)
     end
@@ -190,7 +185,7 @@ end
 export Na₂O
 
 
-struct MgO <:Compound 
+struct MgO <:OxideCompounds 
     name::AbstractString
     A
     MolarMass
@@ -203,3 +198,54 @@ struct MgO <:Compound
     end
 end
 export MgO
+
+struct FeO <: OxideCompounds
+    name::AbstractString
+    A
+    MolarMass
+    T_range::Vector{Tuple}
+    PieceWise::Bool
+    function FeO(;name = "FERROUS OXIDE",A = [3.908E+01 8.574E-03 -7.325E+05 2.466E+02 0],MolarMass = 71.846,
+        T_range = [(298,1800)],PieceWise = false)
+        @assert size(A,1) == size(T_range,1) 
+        new(name,A,MolarMass,T_range,PieceWise)
+    end
+end
+export FeO
+
+struct K₂O <: OxideCompounds
+    name::AbstractString
+    A
+    MolarMass
+    T_range::Vector{Tuple}
+    PieceWise::Bool
+    function K₂O(;name = "FERROUS OXIDE",A = [7.450E+01 3.965E-02 -2.346E+05 0 0],MolarMass = 94.196,
+        T_range = [(298,2000)],PieceWise = false)
+        @assert size(A,1) == size(T_range,1) 
+        new(name,A,MolarMass,T_range,PieceWise)
+    end
+end
+export K₂O
+
+
+struct TiO₂ <: OxideCompounds
+    name::AbstractString
+    A
+    MolarMass
+    T_range::Vector{Tuple}
+    PieceWise::Bool
+    function TiO₂(name = "RUTILE";A = [8.462E+01 5.990E-04 -1.101E+06 -2.957E+02 0],MolarMass = 79.879,
+        T_range = [(298,2100)],PieceWise = false)
+        if name == "RUTILE"
+            @assert size(A,1) == size(T_range,1) 
+            return new(name,A,MolarMass,T_range,PieceWise)
+        end
+        if name == "ANATASE"
+            A = [4.396E+01 1.374E-02 -2.595E+06 6.294E+02 0]
+            T_range = [(298,1300)]
+            @assert size(A,1) == size(T_range,1) 
+            return new(name,A,MolarMass,T_range,PieceWise)
+        end
+    end
+end
+export TiO₂
