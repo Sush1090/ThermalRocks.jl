e15ce355cb207b52b6d9be01ad5463bc755582ba
Sush1090 <sushrut1090@gmail.com>
2024-07-22 13:52:05 +0200

    Multiple dispatch SpecificHeat function

--- src/Utils.jl
+++ src/Utils.jl
@@ -17,17 +17,34 @@ The Shotomate equation computes the specific heat of a given compound at a given
     Cₚ = A₁ + A₂T + A₃/T² + A₄/√T + A₅T²
 """
 function Shotomate(coeff::Vector,T)
-    Tvec = [1,T,T^-2,1/sqrt(T),T^2]
+    Tvec = [1,T,1/T^2,1/sqrt(T),T^2]
     return coeff'*Tvec
 end
+
 
 function SpecificHeat(comp::Compound,T)
         idx = FindFirstIdx(T,comp.T_range)
         A = comp.A[idx,:];
-        return Shotomate(A,T)*(1000/comp.MolarMass)
+        return Shotomate(A,T)*(1000/comp.MolarMass) #convertes to J/kg/K
+end
+
+function SpecificHeat(rock::Rocks,T)
+    @assert T in Interval(rock.T_range[1][1],rock.T_range[1][2])
+    Tvec = [1,1e-4T, 1e4/T^2, 1/sqrt(T)];
+    A = rock.A[1,:];
+    return (Tvec'*A)*1000 # converts to K/kg/K
+end
+
+function SpecificHeat(comp::CustomComposition,T)
+    cp = similar(comp.weights);
+    for i in eachindex(cp)
+        cp[i] = SpecificHeat(comp.compounds[i],T)
+    end
+    return cp'*comp.weights;
 end
 
 
+export SpecificHeat
 # function SpecificHeat(rock::Rocks,T)
 
 # end
